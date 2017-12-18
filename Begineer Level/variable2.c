140e924e8a428924c2df0d162d73cbf4de0a6b20
15EUCS129 <34640776+15EUCS129@users.noreply.github.com>
2017-12-18 15:11:22 +0530

    Create variable2.c

---
+++ Begineer Level/variable2.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+char a[30]="Sachin Ramesh Tendulkar";
+char b[20]="Little Master";
+int r=18426;
+float avrg=44.83;
+int m=463;printf("%s",a);
+printf("\n %s",b);
+printf("\n %d",r);
+printf("\n %.2f",avrg);
+printf("\n %d",m);
+return 0;
+}
