66d25ffb6cad77efdf6ffb2897705667748197b4
15EUCS129 <34640776+15EUCS129@users.noreply.github.com>
2017-12-18 19:04:38 +0530

    Create vowel.c

---
+++ Begineer Level/vowel.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main()
+{
+int ch;
+printf("enter the character:");
+scanf("%c",&ch);
+if(ch=='a'||ch=='e'||ch=='i'||ch=='o'||ch=='u')
+{
+printf("is a vowel");
+}
+else
+{
+printf("is a consonent");
+}
+
+
+    
+}
