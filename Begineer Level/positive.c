c699fcac055ebdf6177744890df59a45905c0b9a
15EUCS129 <34640776+15EUCS129@users.noreply.github.com>
2017-12-18 20:55:16 +0530

    Create positive.c

---
+++ Begineer Level/positive.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main()
+{
+int a;
+scanf("%d",&a);
+if(a>0)
+{
+printf(" number is positive");
+
+}
+if(a<0)
+{
+printf(" number is negative");
+}
+if(a==0)
+{
+
+    printf("number is zero");
+}
+
+}
