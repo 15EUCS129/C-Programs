c931f32a2a91f67a29929eec611f370a9a7211de
15EUCS129 <34640776+15EUCS129@users.noreply.github.com>
2017-12-18 22:38:13 +0530

    Create leapyear.c

---
+++ Begineer Level/leapyear.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+
+{
+    int yr;
+    scanf("%d",&yr);
+    if(yr%4==0)
+    {
+        printf("is leap year");
+    }
+    else
+    {
+        printf("is not leap year");
+    }
+}
