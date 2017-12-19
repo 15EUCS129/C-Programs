27bf88c32da37fc5354218807c21ffb12c1e2acc
15EUCS129 <34640776+15EUCS129@users.noreply.github.com>
2017-12-19 12:55:42 +0530

    Create calculator.c

---
+++ Player/calculator.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+int add,sub,mul,mod,a,b;
+scanf("%d%d",&a,&b);
+add=a+b;
+printf("%d",add);
+sub=a-b;
+printf("%d",sub);
+mul=a*b;
+printf("%d",mul);
+mod=a%b;
+printf("%d",mod);
+}
