f296a3841a6b63b90ec27ba67a54bac80e876be0
15EUCS129 <34640776+15EUCS129@users.noreply.github.com>
2018-03-08 09:43:52 +0530

    Update prime.c

--- Code katta/begineer/set2/prime.c
+++ Code katta/begineer/set2/prime.c
@@ -1,15 +1,15 @@
 #include <stdio.h>
 int main()
 {
-    int n, i, flag = 0;
+    int a, i, flag = 0;
 
     printf("Enter a positive integer: ");
-    scanf("%d",&n);
+    scanf("%d",&a);
 
     for(i=2; i<=n/2; ++i)
     {
        
-        if(n%i==0)
+        if(a%i==0)
         {
             flag=1;
             break;
@@ -17,9 +17,9 @@ int main()
     }
 
     if (flag==0)
-        printf("%d is a prime number.",n);
+        printf("yes",a);
     else
-        printf("%d is not a prime number.",n);
+        printf("no",a);
     
     return 0;
 }
