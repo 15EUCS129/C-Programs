94dfa827a0df997a915c2575e862e1e53a46d3c4
15EUCS129 <34640776+15EUCS129@users.noreply.github.com>
2018-03-08 10:23:31 +0530

    Create prime_range.c

---
+++ Code katta/begineer/set2/prime_range.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+int main()
+{
+   int a, b, flag, i, j;
+ 
+   
+  
+   scanf("%d %d", &a, &b);
+ 
+   for(i=a+1; i<b; ++i)
+   {
+      flag=0;
+      for(j=2; j<=i/2; ++j)
+      {
+         if(i%j==0)
+         {
+            flag=1;
+            break;
+         }
+      }
+      if(flag==0)
+         printf(" %d",i);
+  }
+  return 0;
+}
